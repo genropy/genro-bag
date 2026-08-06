1b13b1ef15f8caa772275e5329a7e033af11bab4
Giovanni Porcari <giovanni.porcari@gmail.com>
2026-08-06 10:10:31 +0200

    chore: release 0.21.0

--- src/genro_bag/__init__.py
+++ src/genro_bag/__init__.py
@@ -24,7 +24,7 @@ from genro_bag.bagnode import BagNode, BagNodeContainer, BagNodeException
 from genro_bag.datachange import DataChangeCollector
 from genro_bag.resolver import BagResolver, BagSyncResolver
 
-__version__ = "0.20.2"
+__version__ = "0.21.0"
 
 # Bag is a TYTX custom type under suffix "X", its historical GenroPy datatype.
 # This makes a Bag carried inside a plain dict or list value survive to_tytx /
